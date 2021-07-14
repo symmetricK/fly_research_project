97963f7f4fea0f2d31b25f99762e6dd343ffde42
Timothy Warren <timlwarren@gmail.com>
2021-07-14 15:27:28 -0700

    modify for new ver. bypass command(line 6,14,18,28)

--- run_trapcam_analysis.py
+++ run_trapcam_analysis.py
@@ -3,16 +3,20 @@ import sys
 #import trapcam_analysis_old as t
 import trapcam_analysis as t
 
-dir = input("Enter the experiment directory you'd like to analyze (e.g. '2017_10_26'): ")
+###dir = input("Enter the experiment directory you'd like to analyze (e.g. '2017_10_26'): ")
 # dir = sys.argv[1]
+dir='2017_10_26'
+
 
 print ('')
 while True:
     analyze_trap_list = []
-    letter = input("Enter a trap letter to analyze: ")
+###    letter = input("Enter a trap letter to analyze: ")
+    letter='G'
     analyze_trap_list.append('trap_'+letter)
     while True:
-        letter = input("Enter another trap letter to analyze, or enter 'go' to start batch analysis: ")
+###        letter = input("Enter another trap letter to analyze, or enter 'go' to start batch analysis: ")
+        letter='go'
         if letter == 'go':
             break
         else:
@@ -21,7 +25,8 @@ while True:
     print ('you said you want to analyze: ')
     for an_trap in analyze_trap_list:
         print (an_trap)
-    user_go_ahead = input("Are those the traps you'd like to analyze? (y/n) ")
+###    user_go_ahead = input("Are those the traps you'd like to analyze? (y/n) ")
+        user_go_ahead='y'
     if user_go_ahead == 'y':
         break
     if user_go_ahead == 'n':
@@ -30,7 +35,9 @@ print ('')
 
 calculate_threshold = False
 calculate_final = False
-thresh_or_final = input("Do you want to analyze just a subset of frames to determine the best in-trap/on-trap threshold, or do you want to do the final analysis? (threshold/final) ")
+###thresh_or_final = input("Do you want to analyze just a subset of frames to determine the best in-trap/on-trap threshold, or do you want to do the final analysis? (threshold/final) ")
+thresh_or_final='final'
+
 if thresh_or_final =='threshold':
     calculate_threshold = True
 if thresh_or_final == 'final':

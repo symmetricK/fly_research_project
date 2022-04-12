4357880993e7f5802d3b5c7ac366ce1c6bc9796f
Timothy Warren <timlwarren@gmail.com>
2022-04-12 11:49:11 -0700

    some update

--- crop.py
+++ crop.py
@@ -6,28 +6,41 @@ import os
 import glob
 
 # to get row and col
-image_dir="/home/flyranch/field_data_and_analysis_scripts/2021lab/trapcam_timelapse/40frames/14-02-35.980501.jpg"
+image_dir="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/220317_WT_80frames/14-02-12.477627.jpg"
 image=cv2.imread(image_dir)
 row,col,color=np.shape(image)
 
 # to read through one folder
-path="/home/flyranch/field_data_and_analysis_scripts/2021lab/trapcam_timelapse/40frames"
+path="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/220317_WT_80frames"
 data_path=os.path.join(path,'*g')
 files=glob.glob(data_path)
 sorted_files=sorted(files)
 
 
-# cropped images for jpg
-path1="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/cropped_40frames_jpg/"
-path2="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/cropped_40frames_png/"
+# they are none type of objects
+path1="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/220317_WT_80frames/cropped_80frames_jpg/"
+path2="/home/flyranch/field_data_and_analysis_scripts/2021lab/refs_for_presentation/220317_WT_80frames/cropped_80frames_png/"
 
 count=1
 for file in sorted_files:
 	img=cv2.imread(file)
 	cropped_img=img[int(row/2)+300:int(row*3/4)+200,int(col/2)+100:int(col*3/4)-150]
-	cv2.imwrite(path1+str(count)+".jpg",cropped_img)
-	cv2.imwrite(path2+str(count)+".png",cropped_img)
+#	pdb.set_trace()
+	if count<10:
+		cv2.imwrite(path1+"00"+str(count)+".jpg",cropped_img)
+		cv2.imwrite(path2+"00"+str(count)+".png",cropped_img)
+	else:
+		cv2.imwrite(path1+"0"+str(count)+".jpg",cropped_img)
+		cv2.imwrite(path2+"0"+str(count)+".png",cropped_img)		
 	count=count+1
+
+
+
+# to conevert pngs to mp4 file
+
+#ffmpeg -r 10 -pattern_type glob -i '*.png' -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
+
+
 
 # read all jpgs
 #data_path1=os.path.join(path1,'*g')
--- file_rename.py
+++ file_rename.py
@@ -2,36 +2,53 @@ import numpy as np
 import os
 import glob
 import pdb
-import re
 
-before_dir=input("Enter a directory which has files you'd like to rename: ")
+before_dir=input("Enter a directory name (without trap_) which has files you'd like to rename: ")
 b_dir="/home/flyranch/field_data_and_analysis_scripts/2021lab/trapcam_timelapse/trap_"+before_dir
 data_path=os.path.join(b_dir,'*g')
 filelist=glob.glob(data_path)
+sorted_files=sorted(filelist)
+
+date=input("Enter what date did you do this experiment (e.g. 20220317 (yyyymmdd)): ")
+files=glob.glob(data_path)
+sorted_files=sorted(files)
 
 count=1
 
-date=input("Enter what date did you do this experiment (e.g. 20220317 (yyyymmdd)): ")
 
-
-
-for file in filelist:
-	if count<10:
-		c="000"+str(count)
-	elif 10<=count<100:
-		c="00"+str(count)
-	elif 100<=count<1000:
-		c="0"+str(count)
-	else:
-		c=str(count)
-	pre="tl_0000_"+c+"_"
+for file in sorted_files:
 	mid=date+"_"
-
 	dis=len(b_dir)
 	time=file[dis+1:dis+3]+file[dis+4:dis+6]+file[dis+7:dis+9]
+	t=time[:2]+"-"+time[2:4]+"-"+time[4:6]
 	ext=".jpg"
-	filename=pre+mid+time+ext
 	pdb.set_trace()
+	test=glob.glob(b_dir+"/"+t+"*"+ext)
+
+	if os.path.exists(b_dir+"/"+time+"*"+ext):
+		if os.path.exists(b_dir+"/"+t+"*"+ext):
+			print(b_dir+"/"+t+"*"+ext)
+			os.remove(b_dir+"/"+t+"*"+ext)
+
+(b_dir+"/"+t+"*"+ext)
+
+
+	else:
+		if count<10:
+			c="000"+str(count)
+		elif 10<=count<100:
+			c="00"+str(count)
+		elif 100<=count<1000:
+			c="0"+str(count)
+		else:
+			c=str(count)
+		pre="tl_0000_"+c+"_"
+
+		filename=pre+mid+time+ext
+		os.rename(file,b_dir+"/"+filename)
+		count=count+1
+
+#	pdb.set_trace()
 
 
 
